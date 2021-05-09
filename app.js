3c06645f533a0d7e346be67f44462551419f10c3
ln-dev7 <leonelngoya@gmail.com>
2021-05-09 05:08:23 +0200

    Premiere partie du container

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 //import * as ___ from '___;
-
+// Gestion du header au scroll
+window.addEventListener('scroll', (e)=>{
+    let header = document.querySelector('.header');
+    let header_arrow = document.querySelector('.header-arrow');
+    let header_burger = document.querySelector('.header-burger');
+    header.classList.toggle('sticky', window.scrollY > 0);
+    header_arrow.classList.toggle('arrow-hide', window.scrollY > 0);
+    header_burger.classList.toggle('burger-show', window.scrollY > 0);
+})
 // Le rond qui suit le souris
 const cursor = document.querySelector('.cursor');
 const cursor2 = document.querySelector('.cursor2');
