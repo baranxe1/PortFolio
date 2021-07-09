bfe85fb3caf0a9b4a1e4cbb97a31410ab27daf6a
LN Dev <62269693+ln-dev7@users.noreply.github.com>
2021-07-09 13:11:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -33,29 +33,41 @@ function loader(){
 
 //
 
+// Menu burger 
+
+const burger = document.querySelector(".header-burger");
+const menu = document.querySelector(".header-nav");
+
+burger.addEventListener("click", ()=>{
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
 //
 
-const textAnim = document.querySelector('h1');
+//
 
-new Typewriter(textAnim, {
-    loop: true,
-    //deleteSpeed: 20 // Vitesse a laquelle le text s'efface
-})
-//.changeDelay(20) 
-.typeString("Hey moi c'est LN,")
-.pauseFor(1000)
-.typeString('<span style="color: #eb0404"> Dev FrontEnd.</span>')
-.pauseFor(1000)
-.deleteChars(13)
-.typeString('<span style="color: #04eb51"> Passioné de la programmation.</span>')
-.pauseFor(1000)
-.deleteChars(30)
-.typeString('<span style="color: #04ebdf"> Gamer PES.</span>')
-.pauseFor(1000)
-.deleteChars(10)
-.typeString('<span style="color: #edf104"> super social :).</span>')
-.pauseFor(10000)
-.start()
+// const textAnim = document.querySelector('h1');
+
+// new Typewriter(textAnim, {
+//     loop: true,
+//     //deleteSpeed: 20 // Vitesse a laquelle le text s'efface
+// })
+// //.changeDelay(20) 
+// .typeString("Hey moi c'est LN,")
+// .pauseFor(1000)
+// .typeString('<span style="color: #eb0404"> Dev FrontEnd.</span>')
+// .pauseFor(1000)
+// .deleteChars(13)
+// .typeString('<span style="color: #04eb51"> Passioné de la programmation.</span>')
+// .pauseFor(1000)
+// .deleteChars(30)
+// .typeString('<span style="color: #04ebdf"> Gamer PES.</span>')
+// .pauseFor(1000)
+// .deleteChars(10)
+// .typeString('<span style="color: #edf104"> super social :).</span>')
+// .pauseFor(10000)
+// .start()
 
 //
 
